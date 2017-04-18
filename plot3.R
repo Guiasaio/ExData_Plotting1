9a13afb178420d84089e1113228193362d4f4de9
Guiasaio <guilherme_aoliveira@honda.com.br>
2017-04-18 08:50:45 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -24,6 +24,7 @@ par(mfrow=c(1,1))
 plot(HPC$Date, HPC$Sub_metering_1, type = "l", xlab = "Date", ylab = "Sub metering")
 lines(HPC$Date, HPC$Sub_metering_2, col = "red")
 lines(HPC$Date, HPC$Sub_metering_3, col = "blue")
+legend('topright', legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c('black', 'red', 'blue'), lty = 1)
 dev.set(which = 2)
 #Prints plot from current device (RStudioGD) to selected device
 dev.print(device = png, file = "plot3.png", width = 480, height = 480)
